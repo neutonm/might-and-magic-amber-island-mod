a9eb88e439371debbdf07b0e78d4d0e688ae5443
neutonm <neutonm@gmail.com>
2025-09-10 16:04:06 +0300

    Feature: manifest.lua (mod data)
    Provides information about amber island mod, potentially could be used by 3rd parties.

---
+++ Scripts/manifest.lua
@@ -0,0 +1,11 @@
+-- FILE:        manifest.lua
+-- DESCRIPTION: Amber Island Mod information
+
+return {
+    name    = "Amber Island",
+    game    = "mm7",
+    version = "v1.1.0",
+    author  = "Henrik Chukhran",
+    website = "https://mightandmagicmod.com",
+    updated = "10.09.2025"
+}
