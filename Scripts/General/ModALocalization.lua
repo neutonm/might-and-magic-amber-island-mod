ef7106f92118382f600aae8b589911262ec5c67c
neutonm <neutonm@gmail.com>
2026-04-27 00:16:32 +0300

    File: ModALocalization.lua -> Adds ModTxt table for common words

---
+++ Scripts/General/ModALocalization.lua
@@ -0,0 +1,62 @@
+ModTxt                  = LocalizeAll{
+    CNull               = " ",
+
+    CDoor               = "Door",
+    CDoubleDoor         = "Double Door",
+    CChest              = "Chest",
+    CLever              = "Lever",
+    CSwitch             = "Switch",
+    CButton             = "Button",
+    CLift               = "Lift",
+    CKeyhole            = "Keyhole",
+    CLockedDoor         = "The Door Is Locked",
+    CLockedChest        = "The Chest Is Locked",
+
+    CFurniture          = "Furniture",
+    CBookcase           = "Bookcase",
+    CCabinet            = "Cabinet",
+    CWineRack           = "Wine Rack",
+
+    CFountain           = "Fountain",
+    CDrinkFountain      = "Drink from the Fountain",
+    CRefreshing         = "Refreshing",
+    CNotRefreshing      = "Not Very Refreshing",
+
+    CTeleport           = "Teleportation Pedestal",
+    CTeleportPlatform   = "Teleportation Platform",
+    CPortalActivated    = "Portal Is Activated",
+
+    CSafeRoom           = "Safe Room",
+    CLeaveDungeon       = "Leave Dungeon",
+    CEnter              = "Enter",
+    CExit               = "Leave",
+
+    CHole               = "Hole",
+    CNothing            = "Nothing Happens",
+
+    CGoldVein           = "Gold Vein",
+
+    CResidence          = "Residence",
+    CHouse              = "House",
+    CTent               = "Tent",
+
+    CStatue             = "Statue",
+    CAltar              = "Altar",
+
+    CTree               = "Tree",
+    CFlower             = "Flower",
+
+    CBoat               = "Boat",
+
+    CWell               = "Well",
+    CDrinkWell          = "Drink from the Well",
+
+    CTrigger            = "Trigger",
+    CVault              = "Vault",
+
+    CSuccessDisarm      = "You Successfully disarm the trap",
+
+    CNeedTeleportItem   = "You need teleportation stone.",
+
+    CAlreadyPressed     = "Somebody already pressed the button!"
+}
