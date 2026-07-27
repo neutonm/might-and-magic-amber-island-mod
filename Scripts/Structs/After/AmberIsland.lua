f9c6c5b8cc17668511509f84d767d06a7c0e709b
neutonm <neutonm@gmail.com>
2026-07-27 17:03:20 +0300

    Fix: show NPC 'Greeting' text within service buildings (shop/guild/bank/etc)

--- Scripts/Structs/After/AmberIsland.lua
+++ Scripts/Structs/After/AmberIsland.lua
@@ -4,7 +4,7 @@ Author:         Henrik Chukhran, 2022 - 2026
 ]]
 
 ------------------------------------------------------------------------------
--- START DATE
+-- DATES & TIME
 ------------------------------------------------------------------------------
 
 local BaseYear              = 992
@@ -24,3 +24,10 @@ mem.u4[0x491824] = StartTime -- new game initial Game.Time
 mem.u4[0x4BF9ED] = StartTime -- victory screen elapsed-time subtraction
 mem.u4[0x432D65] = ArenaReturnTravelTime -- replace the hard-coded four-day return from D05
 mem.IgnoreProtection(false)
+
+------------------------------------------------------------------------------
+-- HOUSES & NPC
+------------------------------------------------------------------------------
+
+-- Removes hardcoded check that hides resident NPC greetings in service buildings
+mem.nop(0x4B2B5C, 6)
